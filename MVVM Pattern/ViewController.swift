0ff3ef58536bee577b1a7fdac231d6c516c88e75
boztopuz <boztopuz@gmail.com>
2023-06-01 12:11:38 +0300

    Initial Commit

---
+++ MVVM Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Pattern
+//
+//  Created by Burak Öztopuz on 1.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
